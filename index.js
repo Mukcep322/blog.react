0c526cfc10950482bddffd50aae948871684cbf9
Mukcep322 <antongolosov48@gmail.com>
2025-06-14 20:04:05 +0300

    full user Authorization

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import mongoose from "mongoose"
-import {registerValidation} from "./validations/auth.js"
+import {registerValidation, loginValidation} from "./validataion.js"
 import checkAuth from "./utils/checkAuth.js"
 
 import * as userController from "./controllers/userController.js"
@@ -19,7 +19,7 @@ app.get("/", (res) => {
   res.send("Hello World")
 })
 
-app.post("/auth/login", userController.login)
+app.post("/auth/login", loginValidation, userController.login)
 app.post("/auth/register", registerValidation, userController.register)
 app.get("/auth/me", checkAuth, userController.getMe)
 
--- validataion.js
+++ validataion.js
@@ -9,3 +9,11 @@ export const registerValidation = [
   body("fullName", "name is required").isLength({min: 3}),
   body("avatarUrl", "Uncorrect url").optional().isURL()
 ]
+
+export const loginValidation = [
+  body("email", "Email must be valid").isEmail(),
+  body(
+    "password",
+    "Password must be longer than 5 and shorter than 32"
+  ).isLength({min: 5, max: 32})
+]
